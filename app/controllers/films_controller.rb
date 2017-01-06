78a72b68058696cde5fb3602be35f250cfd347b5
anhbabiran <anhbabiran@gmail.com>
2017-01-06 21:01:08 +0700

    Add FilmsController

---
+++ app/controllers/films_controller.rb
@@ -0,0 +1,28 @@
+class FilmsController < ApplicationController
+  def index
+  end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    @film = Film.new(film_params)
+    if @film.save
+      redirect_to @film
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @film = Film.find(params[:id])
+  end
+
+  private
+    def film_params
+        params.require(:film).permit(:name,:description,:director,:country)
+    end
+
+
+end
