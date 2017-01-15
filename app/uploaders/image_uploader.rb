2c2976bdb53d2c47897ad8a29bffbcf4ad4b9e0f
anhbabiran <anhbabiran@gmail.com>
2017-01-15 23:05:03 +0700

    Fix 2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
+
+  process :resize_to_fit => [424, 424]
 
   # Choose what kind of storage to use for this uploader:
 
