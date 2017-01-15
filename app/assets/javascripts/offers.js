a58624ad7727159b0ee754867f9ed809f343d1a0
baohot9 <hachan.bt@gmail.com>
2017-01-16 03:50:31 +0700

    Javascript

--- app/assets/javascripts/offers.js
+++ app/assets/javascripts/offers.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+<script>
+function onMouseOver(row){
+  debugger
+  row.style.backgroundColor = "red"
+}
+</script>
